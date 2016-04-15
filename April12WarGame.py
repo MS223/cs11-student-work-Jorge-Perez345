ec9e5b5881f1045fecc13ec7a34e86844c03d00e
Jorge-Perez345 <jorge.perez@ms223.org>
2016-04-15 08:42:28 -0400

    Update April12WarGame.py

--- April12WarGame.py
+++ April12WarGame.py
@@ -1,6 +1,16 @@
 import random
 
 def shuffled_deck():
-	basic_deck = range(2, 15) * 4
-	random.shuffle(basic_deck)
-	return basic_deck
+	deck = range(2, 15) * 4
+	random.shuffle(deck)
+	return deck
+
+deck = shuffled_deck()
+
+def player_turn(player_name):
+    card = deck.pop()
+    print player_name + " drew a " + str(card)
+    return str(card)
+
+while deck:
+    if player_turn(player1):
