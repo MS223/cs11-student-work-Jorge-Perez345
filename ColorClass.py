218537c90889914b84864684b8c922cc3a02aeca
Jorge-Perez345 <jorge.perez@ms223.org>
2016-05-24 09:29:05 -0400

    Create ColorClass.py

---
+++ ColorClass.py
@@ -0,0 +1,19 @@
+class color():
+    """Represents all different colors"""
+
+bloodred = color()
+chakrablue = color()
+gold = color()
+
+bloodred.r = 139
+bloodred.g = 0
+bloodred.b = 0
+
+chakrablue.r = 0
+chakrablue.g = 191
+chakrablue.b = 255
+
+gold.r = 255
+gold.g = 215
+gold.b = 0
+
